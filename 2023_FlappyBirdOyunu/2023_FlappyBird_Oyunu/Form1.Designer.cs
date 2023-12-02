0b1123d956c2143653123b78f838766028677793
Necati <141543993+necatidalar@users.noreply.github.com>
2023-12-02 12:38:56 +0300

    Add files via upload

---
+++ 2023_FlappyBirdOyunu/2023_FlappyBird_Oyunu/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace _2023_FlappyBird_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flappyBird = new System.Windows.Forms.PictureBox();
+            this.BoruUst = new System.Windows.Forms.PictureBox();
+            this.BoruAlt = new System.Windows.Forms.PictureBox();
+            this.zemin = new System.Windows.Forms.PictureBox();
+            this.skorText = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruUst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruAlt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zemin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flappyBird
+            // 
+            this.flappyBird.Image = ((System.Drawing.Image)(resources.GetObject("flappyBird.Image")));
+            this.flappyBird.Location = new System.Drawing.Point(12, 207);
+            this.flappyBird.Name = "flappyBird";
+            this.flappyBird.Size = new System.Drawing.Size(110, 96);
+            this.flappyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.flappyBird.TabIndex = 0;
+            this.flappyBird.TabStop = false;
+            // 
+            // BoruUst
+            // 
+            this.BoruUst.Image = global::_2023_FlappyBird_Oyunu.Properties.Resources.pipedown;
+            this.BoruUst.Location = new System.Drawing.Point(465, -3);
+            this.BoruUst.Name = "BoruUst";
+            this.BoruUst.Size = new System.Drawing.Size(106, 146);
+            this.BoruUst.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoruUst.TabIndex = 1;
+            this.BoruUst.TabStop = false;
+            // 
+            // BoruAlt
+            // 
+            this.BoruAlt.Image = ((System.Drawing.Image)(resources.GetObject("BoruAlt.Image")));
+            this.BoruAlt.Location = new System.Drawing.Point(380, 347);
+            this.BoruAlt.Name = "BoruAlt";
+            this.BoruAlt.Size = new System.Drawing.Size(106, 154);
+            this.BoruAlt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoruAlt.TabIndex = 2;
+            this.BoruAlt.TabStop = false;
+            // 
+            // zemin
+            // 
+            this.zemin.Image = ((System.Drawing.Image)(resources.GetObject("zemin.Image")));
+            this.zemin.Location = new System.Drawing.Point(-2, 498);
+            this.zemin.Name = "zemin";
+            this.zemin.Size = new System.Drawing.Size(639, 78);
+            this.zemin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.zemin.TabIndex = 3;
+            this.zemin.TabStop = false;
+            // 
+            // skorText
+            // 
+            this.skorText.AutoSize = true;
+            this.skorText.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.skorText.Location = new System.Drawing.Point(6, 581);
+            this.skorText.Name = "skorText";
+            this.skorText.Size = new System.Drawing.Size(79, 31);
+            this.skorText.TabIndex = 4;
+            this.skorText.Text = "Skor:";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(513, 596);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "@necatidalar_";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(634, 621);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.skorText);
+            this.Controls.Add(this.zemin);
+            this.Controls.Add(this.BoruAlt);
+            this.Controls.Add(this.BoruUst);
+            this.Controls.Add(this.flappyBird);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(650, 660);
+            this.MinimumSize = new System.Drawing.Size(650, 660);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flappy Bird Oyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruUst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruAlt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zemin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox flappyBird;
+        private System.Windows.Forms.PictureBox BoruUst;
+        private System.Windows.Forms.PictureBox BoruAlt;
+        private System.Windows.Forms.PictureBox zemin;
+        private System.Windows.Forms.Label skorText;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 2023_FlappyBirdOyunu/2023_FlappyBird_Oyunu/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace _2023_FlappyBird_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        int boruHizi = 8;
+        int gravity = 15;
+        int skor = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            flappyBird.Top += gravity;
+            BoruAlt.Left -= boruHizi;
+            BoruUst.Left -= boruHizi;
+            skorText.Text = "Skor: " + skor;
+            if (BoruAlt.Left < -150)
+            {
+                BoruAlt.Left = 800;
+                skor++;
+            }
+            if (BoruUst.Left < -180)
+            {
+                BoruUst.Left = 950;
+                skor++;
+            }
+            if (flappyBird.Bounds.IntersectsWith(BoruAlt.Bounds)
+                || flappyBird.Bounds.IntersectsWith(BoruUst.Bounds)
+                || flappyBird.Bounds.IntersectsWith(zemin.Bounds))
+            {
+                endGame();
+            }
+            if (skor > 5)
+               
+            {
+                boruHizi = 10;
+            }
+            if (skor > 15)
+            {
+                boruHizi = 15;
+            }
+            if (skor > 20)
+            {
+                boruHizi = 20;
+            }
+            if (skor > 25)
+            {
+                boruHizi = 25;
+            }
+            if (skor > 30)
+            {
+                boruHizi = 30;
+            }
+            if (skor > 40)
+            {
+                boruHizi = 35;
+            }
+            if (skor > 50)
+            {
+                boruHizi = 40;
+            }
+            if (skor > 60)
+            {
+                boruHizi = 50;
+            }
+            if (skor >= 250)
+            {
+                MessageBox.Show("Tebrikler! Oyunu Sonuna Ulaştınız! @necatidalar_ instagram adresimize ulaşarak ödülünüzü talep edebilirsiniz.", "Oyun Bitti", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (flappyBird.Top < -25)
+            {
+                endGame();
+            }
+        }
+
+
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Space)
+            {
+                gravity = -12;
+            }
+        }
+
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 12;
+            }
+        }
+
+        private void endGame()
+        {
+            gameTimer.Stop();
+            DialogResult result=
+            MessageBox.Show("Oyun Bitti! Yeniden Başlamak İstermisin?  Oyunun Sonunda Sana Bir Ödülümüz Var! @necatidalar_", "Game Over", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            
+            if (result==DialogResult.Yes)
+            {
+                Application.Restart();
+            }
+            else if (result==DialogResult.No)
+            {
+                this.Close();
+            }
+        }
+    }
+}
+
+
+
+
